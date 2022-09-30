e987cc7cedb0f05574a9c9a4a790ca88a7169a8d
chenyuchang <464769623@qq.com>
2022-09-30 11:09:24 +0800

    [commit update] add MiguCPSdk plugins

---
+++ Plugins/MiguCPSdk/Source/MiguCPSdk/Private/MiguCPSdkBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MiguCPSdkBPLibrary.h"
+#include "MiguCPSdk.h"
+
+UMiguCPSdkBPLibrary::UMiguCPSdkBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UMiguCPSdkBPLibrary::MiguCPSdkSampleFunction(float Param)
+{
+	return -1;
+}
+
