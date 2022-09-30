a8546f56340075461db621b6e97d572ee8fc1135
chenyuchang <464769623@qq.com>
2022-09-30 10:48:36 +0800

    commit default project

---
+++ Source/MiguCPSdkDemo/MiguCPSdkDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MiguCPSdkDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIGUCPSDKDEMO_API AMiguCPSdkDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
