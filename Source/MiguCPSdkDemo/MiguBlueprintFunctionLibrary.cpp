b1914a27767029f43260ad2bad732850d1ef38ca
zemin3 <464769623@qq.com>
2022-10-01 12:34:21 +0800

    删除 无用文件

--- Source/MiguCPSdkDemo/MiguBlueprintFunctionLibrary.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MiguBlueprintFunctionLibrary.h"
-
-void UMiguBlueprintFunctionLibrary::Migu_Pay()
-{
-	UE_LOG(LogTemp, Warning, TEXT("cyc-------Migu_Pay"));
-}
